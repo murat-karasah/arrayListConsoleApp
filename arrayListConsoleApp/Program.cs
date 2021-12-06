535c09f4b05961d51297ec732d28574eaf40a24e
Murat Karasah <mkarasah@gmail.com>
2021-12-06 09:20:16 +0300

    .

--- arrayListConsoleApp/Program.cs
+++ arrayListConsoleApp/Program.cs
@@ -15,13 +15,20 @@ namespace arrayListConsoleApp
             capacity = A1.Capacity;
             count = A1.Count;
             Console.WriteLine(capacity+"..."+count );
-            ArrayList A2 = new ArrayList();
-            A2.Add("sınıf");
-            A2.Add(5);
-            A2.Add("değer");
-            A2.AddRange(A1);
+            //ArrayList A2 = new ArrayList();
+            //A2.Add("sınıf");
+            //A2.Add(5);
+            //A2.Add("değer");
+            //A2.AddRange(A1);
+            //Console.WriteLine(A2.Count);
+            ////object o1 = A1[2];
+            ////Console.WriteLine(o1);
+            //////A1.RemoveAt(2);
+            //////Console.WriteLine(A1.Count);
 
-            Console.WriteLine(A2.Count);
+            ////////A1.Reverse(); //indexleri değerler ile ters çevirir
+            
+             
 
 
         }
